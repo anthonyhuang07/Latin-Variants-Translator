a5b504f48e1d409da692d11f6d39534436194506
Anthony Huang <anthonyhuang07@icloud.com>
2022-08-05 12:04:29 -0400

    a

---
+++ scripts/stuffs.js
@@ -0,0 +1,87 @@
+function pigTranslator(stringer) {
+    let stringArray = stringer.split(" ");
+    console.log(stringArray);
+    for (let a in stringArray) {
+        let currentArray = [...stringArray[a]];
+        currentArray = currentArray.slice(0, -2);
+        currentArray.unshift(currentArray.pop());
+        if (currentArray.at(-1) === '-') {
+            currentArray.pop();
+        }
+        console.log(currentArray);
+        stringArray[a] = currentArray.join('');
+        console.log(stringArray);
+    }
+    stringer = stringArray.join(' ');
+    return stringer;
+}
+/**
+ * Test Line:
+ * asparagus is cool
+ * Test1: eggaspeggareggageggus eggis ceggool
+ * Test2: eggaspeggareggageggus eggis ceggool
+ */
+function pigTranslator2(stringer) {
+    stringer = stringer.toLowerCase();
+    let stringArray = stringer.split(" ");
+    const vowels = /[aeiou]/gi;
+    const punctuationReg = /[!#$%&()*+?,-./:;<=>@[\]^_`{|}~]/g;
+    console.log(stringArray);
+    for (let a in stringArray) {
+        let currentArray = [...stringArray[a]];
+        if (currentArray[0].match(vowels)) {
+            if (currentArray.at(-1).match(punctuationReg)) {
+                let punctuation = currentArray.pop();
+                currentArray.push("yay");
+                currentArray.push(punctuation);
+            }
+            else {
+                currentArray.push("yay");
+            }
+        }
+        else {
+            if (currentArray.at(-1).match(punctuationReg)) {
+                let punctuation = currentArray.pop();
+                currentArray.push(currentArray.shift());
+                currentArray.push("ay");
+                currentArray.push(punctuation);
+            }
+            else {
+                currentArray.push(currentArray.shift());
+                currentArray.push("ay");
+            }
+        }
+        stringArray[a] = currentArray.join('');
+        console.log(stringArray);
+    }
+    stringer = stringArray.join(' ');
+    return stringer;
+}
+const pigEngrish = document.getElementById("pigEnglish");
+const pigLatin = document.getElementById("pigLatin");
+const pigBtn = document.getElementById("pigSwitch");
+const engpig = document.getElementById("eng-pig");
+const pigeng = document.getElementById("pig-eng");
+const pigTitle = document.getElementById("pigTitler");
+const pigTitle2 = document.getElementById("pigTitler2");
+function pigCheck(value) {
+    pigLatin.value = pigTranslator2(value);
+}
+function pigCheck2(value) {
+    pigEngrish.value = pigTranslator(value);
+}
+pigBtn.addEventListener('click', pigToggle);
+function pigToggle() {
+    if (engpig.style.display === "none") {
+        engpig.style.display = "block";
+        pigeng.style.display = "none";
+        pigTitle.style.display = "block";
+        pigTitle2.style.display = "none";
+    }
+    else if (engpig.style.display !== "none") {
+        engpig.style.display = "none";
+        pigeng.style.display = "block";
+        pigTitle2.style.display = "block";
+        pigTitle.style.display = "none";
+    }
+}
